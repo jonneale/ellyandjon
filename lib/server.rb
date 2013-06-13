177de8fb392daff69462af018c7db96bc13a0e2c
Jon Neale <jon.neale@uswitch.com>
2013-06-13 22:43:04 +0100

    A

--- lib/server.rb
+++ lib/server.rb
@@ -1,14 +1,5 @@
 class EllyAndJon < Sinatra::Application
   
-  configure :production do
-    use Rack::Static,
-        urls: ['/stylesheets'],
-        root: File.expand_path('../tmp', __FILE__)
-
-    Sass::Plugin.options.merge!(template_location: 'public/stylesheets/sass',
-                                css_location: 'tmp/stylesheets')
-  end
-  
   get '/' do
     haml :index
   end
